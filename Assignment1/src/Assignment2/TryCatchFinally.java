daf04663983c12eb2c90a519c5691a096069653a
Akash M S <147367710+Akashms987@users.noreply.github.com>
2026-03-10 21:55:51 +0530

    Add files via upload

---
+++ Assignment1/src/Assignment2/TryCatchFinally.java
@@ -0,0 +1,16 @@
+package Assignment2;
+public class TryCatchFinally {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+
+        try{
+            int result = x/y;
+            System.out.println("Result : "+result);
+        }catch(ArithmeticException e){
+            System.out.println("Error : it cannot be divided by zero");
+        }finally{
+            System.out.println("Finally block executed");
+        }
+    }
+}
